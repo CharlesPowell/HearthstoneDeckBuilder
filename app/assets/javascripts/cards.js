3602530419281f87e7cb93476cf448e18c4b102c
Charlie Powell <charliepowell1@gmail.com>
2015-05-28 15:30:02 -0500

    adds deck model and migrations

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+app = app || {}
+
+app Card = Backbone.Model.extend();
+app Cards = Backbone.Collection.extend({
+
+});
+
+$(document).ready(function(){
+
+
+  console.log("cards.js loaded...")
+
+
+
+})
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,122 +6,86 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'open-uri'
 require 'json'
+
+
 Card.delete_all
-set = JSON.parse(open("#{Rails.root}/db/AllSets.json").read)
 
-      #puts set[1]
-      #puts set
-      set["Basic"].each do |card|
+  def test
+    set = JSON.parse(open("#{Rails.root}/db/AllSets.json").read)
+    card = set['Goblins vs Gnomes'].first
+    url = 'http://hearthstonehelper.com/'+ card['id'] +'/'
+    doc = Nokogiri::HTML(open(url))
+    item = doc.css("img")[2]
+      src = item[:src]
+      puts src
+      cmd = ('curl ' + src + " > app/assets/images/"+ card['id']+".png")
+      system(cmd)
 
-        puts card
+    Card.create(
+    :name => card['name'].to_s,
+    :cost => card['cost'],
+    :cardType => card['type'],
+    :rarity => card['rarity'],
+    :faction => card['faction'],
+    :race => card['race'],
+    :playerclass => card['playerClass'],
+    :text => card['text'],
+    :inPlayText => card['inPlayText'],
+    :mechanics => card['mechanics'],
+    :flavor => card['flavor'],
+    :artist => card['artist'],
+    :attack => card['attack'],
+    :health => card['health'],
+    :durability => card['durability'],
+    :blizzid => card['id'],
+    :collectible => card['collectible']
+    )
 
-        Card.create(
-        :name => card['name'].to_s,
-        :cost => card['cost'],
-        :cardType => card['type'],
-        :rarity => card['rarity'],
-        :faction => card['faction'],
-        :race => card['race'],
-        :playerclass => card['playerClass'],
-        :text => card['text'],
-        :inPlayText => card['inPlayText'],
-        :mechanics => card['mechanics'],
-        :flavor => card['flavor'],
-        :artist => card['artist'],
-        :attack => card['attack'],
-        :health => card['health'],
-        :durability => card['durability'],
-        :blizzid => card['id'],
-        :collectible => card['collectible']
-        )
+  end
+
+
+  def make_set(name)
+    set = JSON.parse(open("#{Rails.root}/db/AllSets.json").read)
+    set[name].each do |card|
+      puts card
+      if card['rarity'] != nil && card['collectible'] && card['type'] != 'Hero'
+          url = 'http://hearthstonehelper.com/'+ card['id'] +'/'
+          doc = Nokogiri::HTML(open(url))
+          item = doc.css("img")[2]
+          	src = item[:src]
+          	puts src
+          	cmd = ('curl ' + src + " > app/assets/images/"+ card['id']+".png")
+          	system(cmd)
+
+            sleep(2)
       end
 
-      set["Classic"].each do |card|
+      Card.create(
+      :name => card['name'].to_s,
+      :cost => card['cost'],
+      :cardType => card['type'],
+      :rarity => card['rarity'],
+      :faction => card['faction'],
+      :race => card['race'],
+      :playerclass => card['playerClass'],
+      :text => card['text'],
+      :inPlayText => card['inPlayText'],
+      :mechanics => card['mechanics'],
+      :flavor => card['flavor'],
+      :artist => card['artist'],
+      :attack => card['attack'],
+      :health => card['health'],
+      :durability => card['durability'],
+      :blizzid => card['id'],
+      :collectible => card['collectible']
+      )
+    end
+  end
 
-        Card.create(
-        :name => card['name'],
-        :cost => card['cost'],
-        :cardType => card['type'],
-        :rarity => card['rarity'],
-        :faction => card['faction'],
-        :race => card['race'],
-        :playerclass => card['playerclass'],
-        :text => card['text'],
-        :inPlayText => card['inPlayText'],
-        :mechanics => card['mechanics'],
-        :flavor => card['flavor'],
-        :artist => card['artist'],
-        :attack => card['attack'],
-        :health => card['health'],
-        :durability => card['durability'],
-        :blizzid => card['id'],
-        :collectible => card['collectible']
-        )
-      end
-      set["Curse of Naxxramas"].each do |card|
-
-        Card.create(
-        :name => card['name'],
-        :cost => card['cost'],
-        :cardType => card['type'],
-        :rarity => card['rarity'],
-        :faction => card['faction'],
-        :race => card['race'],
-        :playerclass => card['playerclass'],
-        :text => card['text'],
-        :inPlayText => card['inPlayText'],
-        :mechanics => card['mechanics'],
-        :flavor => card['flavor'],
-        :artist => card['artist'],
-        :attack => card['attack'],
-        :health => card['health'],
-        :durability => card['durability'],
-        :blizzid => card['id'],
-        :collectible => card['collectible']
-        )
-      end
-      set["Goblins vs Gnomes"].each do |card|
-
-        Card.create(
-        :name => card['name'],
-        :cost => card['cost'],
-        :cardType => card['type'],
-        :rarity => card['rarity'],
-        :faction => card['faction'],
-        :race => card['race'],
-        :playerclass => card['playerclass'],
-        :text => card['text'],
-        :inPlayText => card['inPlayText'],
-        :mechanics => card['mechanics'],
-        :flavor => card['flavor'],
-        :artist => card['artist'],
-        :attack => card['attack'],
-        :health => card['health'],
-        :durability => card['durability'],
-        :blizzid => card['id'],
-        :collectible => card['collectible']
-        )
-      end
-      set["Blackrock Mountain"]  .each do |card|
-
-        Card.create(
-        :name => card['name'],
-        :cost => card['cost'],
-        :cardType => card['type'],
-        :rarity => card['rarity'],
-        :faction => card['faction'],
-        :race => card['race'],
-        :playerclass => card['playerclass'],
-        :text => card['text'],
-        :inPlayText => card['inPlayText'],
-        :mechanics => card['mechanics'],
-        :flavor => card['flavor'],
-        :artist => card['artist'],
-        :attack => card['attack'],
-        :health => card['health'],
-        :durability => card['durability'],
-        :blizzid => card['id'],
-        :collectible => card['collectible']
-        )
-      end
+  make_set("Basic")
+  make_set("Classic")
+  make_set("Curse of Naxxramas")
+  make_set("Goblins vs Gnomes")
+  make_set("Blackrock Mountain")
